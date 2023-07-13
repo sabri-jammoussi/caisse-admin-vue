869b222f104ac40c452f1123d51d725546aeb4d9
sabri-jammoussi <sabrijm123@gmail.com>
2023-07-13 11:22:09 +0100

    adding fuction to do unique code

--- src/components/caisse/AjouterCaisses.vue
+++ src/components/caisse/AjouterCaisses.vue
@@ -1,22 +1,42 @@
 <template>
   <v-sheet width="900" class="mx-auto">
-    <form @submit.prevent="addCaisse"> 
-    <v-text-field 
-    v-model="state.Code" 
-    label="Code" 
-    :error-messages="v$.Code.$errors.map(e => e.$message)"  
-    @blur="v$.Code.touch"
-    @input="v$.Code.$touch"
-    > 
-    </v-text-field>
-    <v-text-field v-model="state.Intitule" label="Intitule" :error-messages="v$?.Intitule?.$errors?.map(e => e.$message)"></v-text-field>
-    <v-select v-model="state.Depot" :items="state.depotOptions" label="Depot" :error-messages="v$?.Depot?.$errors?.map(e => e.$message)"></v-select>
-    <v-select v-model="state.Souches" :items="state.souchesOptions" label="Souches" :error-messages="v$?.Souches?.$errors?.map(e => e.$message)"></v-select>
+    <form @submit.prevent="addCaisse">
+      <v-text-field
+        v-model="state.Code"
+        label="Code"
+        :error-messages="v$.Code.$errors.map((e) => e.$message)"
+        @blur="v$.Code.touch"
+        @input="v$.Code.$touch"
+      >
+      </v-text-field>
+      <v-text-field
+        v-model="state.Intitule"
+        label="Intitule"
+        :error-messages="v$?.Intitule?.$errors?.map((e) => e.$message)"
+        @blur="v$.Intitule.touch"
+        @input="v$.Intitule.$touch"
+      ></v-text-field>
+      <v-select
+        v-model="state.Depot"
+        :items="state.depotOptions"
+        label="Depot"
+        :error-messages="v$?.Depot?.$errors?.map((e) => e.$message)"
+        @blur="v$.Depot.touch"
+    @input="v$.Depot.$touch"
+      ></v-select>
+      <v-select
+        v-model="state.Souches"
+        :items="state.souchesOptions"
+        label="Souches"
+        :error-messages="v$?.Souches?.$errors?.map((e) => e.$message)"
+        @blur="v$.Souches.touch"
+        @input="v$.Souches.$touch"
+      ></v-select>
 
-    <v-btn type ="submit" color="#007bff">Ajouter</v-btn>
-    <router-link to="/ListCaisse">
-      <v-btn color="red">Annuler</v-btn>
-    </router-link>
+      <v-btn type="submit" color="#007bff">Ajouter</v-btn>
+      <router-link to="/ListCaisse">
+        <v-btn color="red">Annuler</v-btn>
+      </router-link>
     </form>
   </v-sheet>
 </template>
@@ -25,8 +45,8 @@
 import axios from "axios";
 import router from "@/router/index";
 import { reactive, onMounted } from "vue";
-import { useVuelidate } from '@vuelidate/core'
-import { required ,helpers } from '@vuelidate/validators'
+import { useVuelidate } from "@vuelidate/core";
+import { required, helpers } from "@vuelidate/validators";
 
 export default {
   name: "ADDCaisse",
@@ -37,18 +57,22 @@ export default {
       Depot: "",
       Souches: "",
       depotOptions: [],
-      souchesOptions: []
+      souchesOptions: [],
     };
 
     const state = reactive({
-        ...initialState,
+      ...initialState,
     });
 
     const rules = {
-      Code: { required : helpers.withMessage('Code is required', required) },
-      Intitule: { required : helpers.withMessage('Intitule is required', required) },
-      Depot: { required : helpers.withMessage('Depot is required', required) },
-      Souches: { required : helpers.withMessage('Souches is required', required) }
+      Code: { required: helpers.withMessage("Code is required", required) },
+      Intitule: {
+        required: helpers.withMessage("Intitule is required", required),
+      },
+      Depot: { required: helpers.withMessage("Depot is required", required) },
+      Souches: {
+        required: helpers.withMessage("Souches is required", required),
+      },
     };
 
     const v$ = useVuelidate(rules, state);
@@ -71,42 +95,48 @@ export default {
       }
     };
 
-    const addCaisse = async () => {
-      try {
-         console.log("+++++++++++++++++++++++++++++V$",v$.value)
-        
-        v$.value.$touch();
-        if (v$.value.$invalid) return; // eli zedo afif
-        
-        const response = await axios.post(
-          "http://localhost:3000/caisses",
-          {
-            Code: state.Code,
-            Intitule: state.Intitule,
-            Depot: state.Depot,
-            Souches: state.Souches
-          }
-        );
-        console.log("okkkkkkkkkkk");
-       
-        alert("Caisse added successfully");
-        state.Code = "";
-        state.Intitule = "";
-        state.Depot = "";
-        state.Souches = "";
-        router.push({ name: "ListCaisse" }); // Redirect to /listcaisse
-      } catch (error) {
-        console.error(error);
-        alert("An error occurred while adding the caisse");
-      }
-    };
-    
+const addCaisse = async () => {
+  try {
+    v$.value.$touch();
+    if (v$.value.$invalid) return;
+
+    // Check if code already exists
+    const response = await axios.get("http://localhost:3000/caisses");
+    const existingCaisse = response.data.find(
+      (caisse) => caisse.Code === state.Code
+    );
+    if (existingCaisse) {
+      alert("Le code existe déjà !! Veuillez entrer un code unique.");
+      return;
+    }
+
+    // Add the caisse
+    await axios.post("http://localhost:3000/caisses", {
+      Code: state.Code,
+      Intitule: state.Intitule,
+      Depot: state.Depot,
+      Souches: state.Souches,
+    });
+
+    state.Code = "";
+    state.Intitule = "";
+    state.Depot = "";
+    state.Souches = "";
+
+    router.push({ name: "ListCaisse" });
+  } catch (error) {
+    console.error(error);
+    alert("An error occurred while adding the caisse");
+  }
+};
+
+
     onMounted(() => {
       getDepotOptions();
       getSouchesOptions();
     });
 
     return { state, v$, addCaisse };
-  }
+  },
 };
 </script>
