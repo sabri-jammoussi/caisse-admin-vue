5e7a89ec52e78cb6c1f57b81a379c1c30db561b6
sabri-jammoussi <sabrijm123@gmail.com>
2023-07-14 12:42:39 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/",
+  base : "/caisse-admin",
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
