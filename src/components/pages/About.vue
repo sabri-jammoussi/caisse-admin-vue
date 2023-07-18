6806a2685e678a6d42e007653957ca1107a49192
sabri-jammoussi <sabrijm123@gmail.com>
2023-07-18 14:28:14 +0100

    adding the rate

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,4 +1,35 @@
 <template>
- <h1>About page </h1>
-
-</template>
+    <div class="container">
+      <div class="content">
+        <v-rating
+          class="rating"
+          v-model="rating"
+          hover
+          half-increments
+        ></v-rating>
+        <pre>{{ rating }}</pre>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data: () => ({
+      rating: 3.5,
+    }),
+  };
+  </script>
+  
+  <style scoped>
+  .container {
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    height: 90vh;
+  }
+  
+  .content {
+    text-align: center;
+  }
+  </style>
+  
