097b8652415998b1017881d2758505dc52002175
sabri-jammoussi <sabrijm123@gmail.com>
2023-07-18 14:27:39 +0100

    modified style of button , adding paginaation ,adding v-progress-circular ...

--- src/components/caisse/ListCaisse.vue
+++ src/components/caisse/ListCaisse.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
     <router-link to="/AjouterCaisse">
-      <v-btn>Ajouter Caisse
-        <v-tooltip activator="parent" location="end">AjouterCaisse</v-tooltip>
-      </v-btn>
+      <v-btn>Ajouter Caisse</v-btn>
     </router-link>
-    <v-table>
+    <v-table class="bordered">
       <thead>
         <tr>
           <th class="text-left">Code</th>
@@ -15,20 +13,28 @@
           <th class="text-left">Actions</th>
         </tr>
       </thead>
-      <tbody v-if="caisses.length > 0">
-        <tr v-for="(item, index) in caisses" :key="index">
+      <tbody v-if="paginatedCaisses.length > 0">
+        <tr v-for="(item, index) in paginatedCaisses" :key="index">
           <td>{{ item.Code }}</td>
           <td>{{ item.Intitule }}</td>
           <td>{{ item.Depot }}</td>
           <td>{{ item.Souches }}</td>
           <td>
             <router-link :to="`/EditCaisse/${item.id}`">
-              <v-btn v-bind="attrs" v-on="on">
-                <v-tooltip activator="parent" location="end">Consulter</v-tooltip>
-                <v-icon class="text-subtitle-1" color="blue" @click.stop="editCaisse(item.id)" style="margin-right: 12px">mdi-magnify</v-icon>
+              <v-btn icon v-bind="props" v-on="on">
+                <v-tooltip activator="parent" location="end"
+                  >Consulter</v-tooltip
+                >
+                <v-icon
+                  class="text-subtitle-1"
+                  color="blue"
+                  @click.stop="editCaisse(item.id)"
+                  style="margin-right: 12px"
+                  >mdi-magnify</v-icon
+                >
               </v-btn>
             </router-link>
-            <v-btn v-bind="attrs" v-on="on" @click.stop="deleteCaisse(item.id)">
+            <v-btn icon v-bind="props"  v-on="on" @click.stop="deleteCaisse(item.id)">
               <v-tooltip activator="parent" location="end">Supprimer</v-tooltip>
               <v-icon class="text-subtitle-1" color="red">mdi-delete</v-icon>
             </v-btn>
@@ -37,10 +43,23 @@
       </tbody>
       <tbody v-else>
         <tr>
-          <td>Loading ...</td>
+          <td colspan="5" class="text-center">
+            <v-progress-circular
+              :size="30"
+              color="primary"
+              indeterminate
+            ></v-progress-circular>
+            Loading ...
+          </td>
         </tr>
       </tbody>
     </v-table>
+
+    <v-pagination
+      v-model="currentPage"
+      :length="totalPages"
+      @input="changePage"
+    ></v-pagination>
 
     <v-dialog v-model="deleteDialog" max-width="400">
       <v-card>
@@ -49,8 +68,9 @@
           Voulez-vous vraiment supprimer cette caisse ?
         </v-card-text>
         <v-card-actions>
-          <v-btn color="primary" text @click="deleteDialog = false">Annuler</v-btn>
+        
           <v-btn color="red" text @click="confirmDelete">Supprimer</v-btn>
+          <v-btn color="primary" text @click="deleteDialog = false">Annuler</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -59,18 +79,30 @@
 
 <script>
 import axios from "axios";
-//test 
+
 export default {
   name: "caisses",
   data() {
     return {
       caisses: [],
+      currentPage: 1,
+      itemsPerPage: 9,
       deleteDialog: false,
       caisseToDelete: null,
     };
   },
   mounted() {
     this.getCaisses();
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.caisses.length / this.itemsPerPage);
+    },
+    paginatedCaisses() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      return this.caisses.slice(startIndex, endIndex);
+    },
   },
   methods: {
     getCaisses() {
@@ -104,6 +136,26 @@ export default {
           this.deleteDialog = false;
         });
     },
+    changePage(page) {
+      this.currentPage = page;
+    },
   },
 };
 </script>
+
+<style scoped>
+.bordered {
+  border-collapse: collapse;
+  border: 1px solid #e0e0e0;
+}
+
+.bordered th,
+.bordered td {
+  border: 1px solid #e0e0e0;
+  padding: 8px;
+}
+
+.text-center {
+  text-align: center;
+}
+</style>
