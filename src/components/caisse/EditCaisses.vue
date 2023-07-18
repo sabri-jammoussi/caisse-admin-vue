6419adc9184480d50f887a1741b521952fbf3016
sabri-jammoussi <sabrijm123@gmail.com>
2023-07-18 12:59:03 +0100

    aading rules to Intitule

--- src/components/caisse/EditCaisses.vue
+++ src/components/caisse/EditCaisses.vue
@@ -3,33 +3,56 @@
     <v-row justify="space-around">
       <v-card width="1000" title="Modifier">
         <v-sheet width="900" class="mx-auto">
-          <v-text-field
-            v-model="model.caisse.Code"
-            :readonly="!editMode || !canEditCode"
-            label="Code"
-          ></v-text-field>
-          <v-text-field
-            v-model="model.caisse.Intitule"
-            :readonly="!editMode"
-            label="Intitule"
-          ></v-text-field>
-          <v-select
-            v-model="model.caisse.Depot"
-            :readonly="!editMode"
-            :items="depotOptions"
-            label="Depot"
-          ></v-select>
-          <v-select
-            v-model="model.caisse.Souches"
-            :readonly="!editMode"
-            :items="souchesOptions"
-            label="Souches"
-          ></v-select>
+          <form @submit.prevent="submitChanges">
+            <v-text-field
+              v-model="model.caisse.Code"
+              :readonly="!editMode || !canEditCode"
+              label="Code"
+            ></v-text-field>
+            <v-text-field
+              v-model="model.caisse.Intitule"
+              :readonly="!editMode"
+              :error-messages="
+                v$?.caisse?.Intitule?.$errors?.map((e) => e.$message)"
+              @blur="v$?.caisse?.Intitule?.$touch"
+              @input="v$?.caisse?.Intitule?.$touch"
+              label="Intitule"
+            ></v-text-field>
 
-          <v-btn v-if="!editMode" @click="enableEditMode" color="primary" class="button-group1">Modifier</v-btn>
-          <v-btn v-else @click="submitChanges" color="success" class="button-group1">Submit</v-btn>
-          <v-btn v-if="!editMode" color="red" @click="goToListCaisse" class="button-group">Retour</v-btn>
-          <v-btn v-else color="red" @click="resetForm" class="button-group">Annuler</v-btn>
+            <v-select
+              v-model="model.caisse.Depot"
+              :readonly="!editMode"
+              :items="depotOptions"
+              label="Depot"
+            ></v-select>
+            <v-select
+              v-model="model.caisse.Souches"
+              :readonly="!editMode"
+              :items="souchesOptions"
+              label="Souches"
+            ></v-select>
+
+            <v-btn
+              v-if="!editMode"
+              @click="enableEditMode"
+              color="primary"
+              class="button-group1"
+              >Modifier</v-btn
+            >
+            <v-btn v-else type="submit" color="success" class="button-group1"
+              >Submit</v-btn
+            >
+            <v-btn
+              v-if="!editMode"
+              color="red"
+              @click="goToListCaisse"
+              class="button-group"
+              >Retour</v-btn
+            >
+            <v-btn v-else color="red" @click="resetForm" class="button-group"
+              >Annuler</v-btn
+            >
+          </form>
         </v-sheet>
       </v-card>
     </v-row>
@@ -39,7 +62,10 @@
 <script>
 import axios from "axios";
 import { ref, onMounted } from "vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, helpers } from "@vuelidate/validators";
 import { useRouter } from "vue-router";
+const { withMessage } = helpers;
 
 export default {
   name: "EditCaisse",
@@ -52,6 +78,16 @@ export default {
         Souches: "",
       },
     });
+
+    const rules = {
+      caisse: {
+        Intitule: {
+          required: withMessage("Intitule obligatoire", required),
+        },
+      },
+    };
+
+    const v$ = useVuelidate(rules, model);
     const caisseOrigin = ref(null);
     const depotOptions = ref([]);
     const souchesOptions = ref([]);
@@ -62,7 +98,9 @@ export default {
 
     async function getCaisseData(Caisseid) {
       try {
-        const res = await axios.get(`http://localhost:3000/caisses/${Caisseid}`);
+        const res = await axios.get(
+          `http://localhost:3000/caisses/${Caisseid}`
+        );
         console.log(res.data);
         const caisseData = res.data;
         caisseOrigin.value = { ...caisseData };
@@ -103,7 +141,10 @@ export default {
     async function submitChanges() {
       const caisseId = router.currentRoute.value.params.id;
       try {
-        await axios.put(`http://localhost:3000/caisses/${caisseId}`, model.value.caisse);
+        v$?.value.$touch();
+        if (v$?.value.$invalid) return;
+        await axios.put(`http://localhost:3000/caisses/${caisseId}`, model.value.caisse );
+
         router.push({ name: "ListCaisse" });
       } catch (error) {
         console.error(error);
@@ -135,22 +176,26 @@ export default {
       submitChanges,
       goToListCaisse,
       resetForm,
+      v$,
     };
   },
 };
 </script>
 
 <style scoped>
-.button-group1{
+.button-group1 {
   align-content: center;
   justify-content: flex-end;
   margin-top: 20px;
 }
-.button-group{
+.button-group {
   align-content: center;
   justify-content: flex-end;
   margin-top: 20px;
   margin-left: 80px;
 }
 
+.v-input__control--outlined.error--text {
+  border-color: red;
+}
 </style>
