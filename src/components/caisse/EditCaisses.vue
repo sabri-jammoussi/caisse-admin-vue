2248865a1254a8bcb2a63b0a68b3aef4b78f38f8
sabri-jammoussi <sabrijm123@gmail.com>
2023-07-18 11:28:05 +0100

    changed option Api to Composition Api and adding method to reset the form to unitial state !

--- src/components/caisse/EditCaisses.vue
+++ src/components/caisse/EditCaisses.vue
@@ -1,120 +1,156 @@
 <template>
-<v-container>
+  <v-container>
     <v-row justify="space-around">
-      <v-card width="1000" title="Modifer"    >
-  <v-sheet width="900" class="mx-auto">
-    <v-text-field
-      v-model="model.caisse.Code"
-      :readonly="!editMode || !canEditCode"
-      label="Code"
-    ></v-text-field>
-    <v-text-field
-      v-model="model.caisse.Intitule"
-      :readonly="!editMode"
-      label="Intitule"
-    ></v-text-field>
-    <v-select
-      v-model="model.caisse.Depot"
-      :readonly="!editMode"
-      :items="depotOptions"
-      label="Depot"
-    ></v-select>
-    <v-select
-      v-model="model.caisse.Souches"
-      :readonly="!editMode"
-      :items="souchesOptions"
-      label="Souches"
-    ></v-select>
+      <v-card width="1000" title="Modifier">
+        <v-sheet width="900" class="mx-auto">
+          <v-text-field
+            v-model="model.caisse.Code"
+            :readonly="!editMode || !canEditCode"
+            label="Code"
+          ></v-text-field>
+          <v-text-field
+            v-model="model.caisse.Intitule"
+            :readonly="!editMode"
+            label="Intitule"
+          ></v-text-field>
+          <v-select
+            v-model="model.caisse.Depot"
+            :readonly="!editMode"
+            :items="depotOptions"
+            label="Depot"
+          ></v-select>
+          <v-select
+            v-model="model.caisse.Souches"
+            :readonly="!editMode"
+            :items="souchesOptions"
+            label="Souches"
+          ></v-select>
 
-    <v-btn v-if="!editMode" @click="enableEditMode" color="primary">Modifier</v-btn>
-<v-btn v-else @click="submitChanges" color="success">Submit</v-btn>
-<router-link to="/ListCaisse">
-  <v-btn color="red">Retour</v-btn>
-</router-link>
-
-  </v-sheet>
-  </v-card>
+          <v-btn v-if="!editMode" @click="enableEditMode" color="primary" class="button-group1">Modifier</v-btn>
+          <v-btn v-else @click="submitChanges" color="success" class="button-group1">Submit</v-btn>
+          <v-btn v-if="!editMode" color="red" @click="goToListCaisse" class="button-group">Retour</v-btn>
+          <v-btn v-else color="red" @click="resetForm" class="button-group">Annuler</v-btn>
+        </v-sheet>
+      </v-card>
     </v-row>
-</v-container>
+  </v-container>
 </template>
 
 <script>
 import axios from "axios";
-import router from "@/router/index";
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
   name: "EditCaisse",
-  data() {
-    return {
-      model: {
-        caisse: {
-          Code: "",
-          Intitule: "",
-          Depot: "",
-          Souches: ""
-        }
+  setup() {
+    const model = ref({
+      caisse: {
+        Code: "",
+        Intitule: "",
+        Depot: "",
+        Souches: "",
       },
-      depotOptions: [],
-      souchesOptions: [],
-      editMode: false,
-      canEditCode: false
-    };
-  },
-  mounted() {
-    this.getCaisseData(this.$route.params.id);
-    this.getDepotOptions();
-    this.getSouchesOptions();
-  },
-  methods: {
-    async getCaisseData(Caisseid) {
+    });
+    const caisseOrigin = ref(null);
+    const depotOptions = ref([]);
+    const souchesOptions = ref([]);
+    const editMode = ref(false);
+    const canEditCode = ref(false);
+
+    const router = useRouter();
+
+    async function getCaisseData(Caisseid) {
       try {
         const res = await axios.get(`http://localhost:3000/caisses/${Caisseid}`);
         console.log(res.data);
-        const caisseData = res.data; // Assuming the response contains the caisse data
-        this.model.caisse = {
+        const caisseData = res.data;
+        caisseOrigin.value = { ...caisseData };
+        model.value.caisse = {
           Code: caisseData.Code,
           Intitule: caisseData.Intitule,
           Depot: caisseData.Depot,
-          Souches: caisseData.Souches
+          Souches: caisseData.Souches,
         };
       } catch (error) {
         console.error(error);
       }
-    },
-    async getDepotOptions() {
+    }
+
+    async function getDepotOptions() {
       try {
         const res = await axios.get("http://localhost:3000/depots");
-        this.depotOptions = res.data.map((depot) => depot.intitule);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    async getSouchesOptions() {
-      try {
-        const res = await axios.get("http://localhost:3000/souches");
-        this.souchesOptions = res.data.map((souche) => souche.intitule);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    enableEditMode() {
-      this.editMode = true;
-      this.canEditCode = false;
-    },
-    async submitChanges() {
-      const caisseId = this.$route.params.id;
-      try {
-        const res = await axios.put(`http://localhost:3000/caisses/${caisseId}`, this.model.caisse);
-        console.log("aa");
-        // alert(res.data.message);
-        router.push({ name: "ListCaisse" }); // Redirect to /listcaisse
+        depotOptions.value = res.data.map((depot) => depot.intitule);
       } catch (error) {
         console.error(error);
       }
     }
-  }
+
+    async function getSouchesOptions() {
+      try {
+        const res = await axios.get("http://localhost:3000/souches");
+        souchesOptions.value = res.data.map((souche) => souche.intitule);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    function enableEditMode() {
+      editMode.value = true;
+      canEditCode.value = false;
+    }
+
+    async function submitChanges() {
+      const caisseId = router.currentRoute.value.params.id;
+      try {
+        await axios.put(`http://localhost:3000/caisses/${caisseId}`, model.value.caisse);
+        router.push({ name: "ListCaisse" });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    function goToListCaisse() {
+      router.push({ name: "ListCaisse" });
+    }
+
+    function resetForm() {
+      model.value.caisse = { ...caisseOrigin.value };
+    }
+
+    onMounted(async () => {
+      const caisseId = router.currentRoute.value.params.id;
+      await getCaisseData(caisseId);
+      await getDepotOptions();
+      await getSouchesOptions();
+    });
+
+    return {
+      model,
+      depotOptions,
+      souchesOptions,
+      editMode,
+      canEditCode,
+      enableEditMode,
+      submitChanges,
+      goToListCaisse,
+      resetForm,
+    };
+  },
 };
 </script>
+
 <style scoped>
+.button-group1{
+  align-content: center;
+  justify-content: flex-end;
+  margin-top: 20px;
+}
+.button-group{
+  align-content: center;
+  justify-content: flex-end;
+  margin-top: 20px;
+  margin-left: 80px;
+}
 
 </style>
