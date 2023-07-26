0d5278ab64163d698971ba8d8efaf4679fba511a
sabri-jammoussi <sabrijm123@gmail.com>
2023-07-26 19:29:46 +0100

    adding the children component to show the login and signup page without navbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,79 +1,90 @@
-import { createRouter, createWebHistory } from "vue-router"
-import Home from   '../components/pages/Home.vue'
-import About from   '../components/pages/About.vue'
-import Listcaisse from '../components/caisse/ListCaisse.vue'
-import Ajoutercaisse from '../components/caisse/AjouterCaisses.vue'
-import Editcaisse from '../components/caisse/EditCaisses.vue'
-import Ajouterpersonnel from '../components/personnel/Ajouterpersonnel.vue'
-import Listpersonnel from '../components/personnel/ListPersonnel.vue'
-import Editpersonnel from '@/components/personnel/EditPersonnel.vue'
-import login from '@/components/register/login.vue'
-import signup from '@/components/register/sign_up.vue'
-import NotFound from   '../components/NotFound.vue'
-import { RouterLink } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
+import Navbar from "@/components/pages/NavBar.vue";
+import Home from "@/components/pages/Home.vue";
+import About from "@/components/pages/About.vue";
+import Listcaisse from "@/components/caisse/ListCaisse.vue";
+import Ajoutercaisse from "@/components/caisse/AjouterCaisses.vue";
+import Editcaisse from "@/components/caisse/EditCaisses.vue";
+import Ajouterpersonnel from "@/components/personnel/Ajouterpersonnel.vue";
+import Listpersonnel from "@/components/personnel/ListPersonnel.vue";
+import Editpersonnel from "@/components/personnel/EditPersonnel.vue";
+import login from "@/components/register/login.vue";
+import signup from "@/components/register/sign_up.vue";
+import app from '@/components/App.vue'
+import NotFound from "@/components/NotFound.vue";
 
-const routes =[
-    {
-    path: '/',
-    name: "Home",
-    component : Home 
+const routes = [
+  {
+    path: "/",
+    component: Navbar,
+    children: [
+      {
+        path: "",
+        name: "Home",
+        component: Home,
+      },
+      {
+        path: "about",
+        name: "About",
+        component: About,
+      },
+      {
+        path: "listcaisse",
+        name: "ListCaisse",
+        component: Listcaisse,
+      },
+      {
+        path: "AjouterCaisse",
+        name: "Ajouter Caisse",
+        component: Ajoutercaisse,
+      },
+      {
+        path: "EditCaisse/:id",
+        name: "EditCaisse",
+        component: Editcaisse,
+      },
+      {
+        path: "listpersonnel",
+        name: "ListPersonnel",
+        component: Listpersonnel,
+      },
+      {
+        path: "ajouterpersonnel",
+        name: "AjouterPersonnel",
+        component: Ajouterpersonnel,
+      },
+      {
+        path: "editpersonnel/:id",
+        name: "EditPersonnel",
+        component: Editpersonnel,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    name: "login",
+    components: {
+      default: app,
+      login: login,
     },
-    {
-        path :'/about',
-        name : "About",
-        component :About
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    components: {
+      default: app,
+      signup: signup,
     },
-    {
-        path : '/listcaisse',
-        name:"ListCaisse",
-        component : Listcaisse,
-        
-    },
-    {
-        path : '/AjouterCaisse',
-        name:"Ajouter Caisse",
-        component : Ajoutercaisse
-    },
-    {
-        path : '/EditCaisse/:id',
-        name:"EditCaisse",
-        component : Editcaisse
-    },
-    {
-        path : '/listpersonnel',
-        name:"ListPersonnel",
-        component : Listpersonnel
-    },
-    {
-        path : '/ajouterpersonnel',
-        name:"AjouterPersonnel",
-        component : Ajouterpersonnel
-    },
-    {
-        path : '/editpersonnel/:id',
-        name:"EditPersonnel",
-        component : Editpersonnel
-    },
-    {
-        path : '/login',
-        name:"login",
-        component : login
-    },
-    {
-        path : '/signup',
-        name:"signup",
-        component : signup
-    },
-    {
-        path: '/:pathMatch(.*)*',
-        component : NotFound
-    },
-  
-]
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    component: NotFound,
+  },
+];
+
 const router = createRouter({
-    history : createWebHistory(),
-    routes,
-    RouterLink,
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
