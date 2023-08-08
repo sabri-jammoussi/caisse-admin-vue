1b6022c3ceee6367e171ca863402f008ab651710
sabri-jammoussi <sabrijm123@gmail.com>
2023-08-08 19:26:43 +0100

    adding the fetch user to show the name and email in the navbar

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,39 @@
-// store.js
 import { defineStore } from 'pinia';
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    user: null,
+    emailuser:null// Provide a default value for the user state
+  }),
+
+  actions: {
+    setEmail(emailuser){
+      this.emailuser=emailuser;
+      console.log("user-------",this.emailuser)
+    },
+    async fetchUser() {
+
+      try {
+        const res = await fetch(`http://localhost:3000/authentification?email=${this.emailuser}`);      
+        console.log("res !!!!!!!!!",res)
+        const user = await res.json();
+        console.log("user loaded",user[0])
+        this.user = user[0];
+        console.log("55555555555555555 store user",this.user)
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      }
+     
+    },
+   
+  },
+});
 
 export const usePhotoStore = defineStore('photo', {
   state: () => ({
     photo: null,
   }),
+
   actions: {
     setPhoto(photo) {
       this.photo = photo;
